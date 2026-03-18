fe899a9457cc20ed65631f1923fdca17b29eff94
AlberGG01 <AlberGG01@users.noreply.github.com>
2026-03-18 18:53:04 +0100

    actualizar.bat: instalar dependencias tras git pull

--- actualizar.bat
+++ actualizar.bat
@@ -1,5 +1,7 @@
 @echo off
 echo Actualizando aplicacion...
 git pull origin main
+echo Instalando dependencias nuevas...
+venv\Scripts\pip install -r requirements.txt -q
 echo Actualizacion completada. Reinicia la aplicacion.
 pause
